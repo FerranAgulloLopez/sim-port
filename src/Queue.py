24bc9b940e41dabd5c3d61d61bc48e4cb52f40b4
George Bochileanu <georgebochileanu@gmail.com>
2019-05-24 18:30:48 +0200

    Changes to None the inputModule of Queue
    Changes to None the inputModule of Queue

--- src/Queue.py
+++ src/Queue.py
@@ -4,7 +4,7 @@ class Queue:
 
     def __init__(self, maxCapacity=0):
         self.maxCapacity = maxCapacity  # 0 = inf (default)
-        self.inputModule = []
+        self.inputModule = None
         self.entitiesList = []
         self.outputList = []
         self.maxQueueLength = 0 # Estadistico
