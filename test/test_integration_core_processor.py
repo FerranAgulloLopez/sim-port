d1ab11bdb33ff4f013603f1cb57109336979186b
George Bochileanu <georgebochileanu@gmail.com>
2019-05-31 18:09:30 +0200

    Update tests
    Update tests

--- test/test_integration_core_processor.py
+++ test/test_integration_core_processor.py
@@ -1,18 +1,43 @@
 from unittest import TestCase
-
+from src.Core import Core
+from src.Queue import Queue
+from src.Processor import Processor
+from src.Entity import Entity
+from src.Constants import Constants
 
 class TestIntegrationCoreProcessor(TestCase):
     def setUp(self):
-        #self.coreObj = Core()
+        self.coreObj = Core()
+        self.processorObj = Processor(self.coreObj)
 
     def tearDown(self):
-        #self.coreObj = None
+        self.coreObj = None
+        self.processorObj = None
 
-    def test_can_host_entity_unlimited_not_busy_output(self):
-        """queueObj2 = Queue(0)
-        queueObj = Queue(0)
-        queueObj.addOutput(queueObj2)
+    def test_isIdle(self):
+        self.assertTrue(self.processorObj.isIdle(), "The processor should be idle")
 
-        queueObj.nextArrival(0)
+    def test_isIdle_notIdle(self):
+        self.processorObj.hostedEntity = 1
+        self.assertFalse(self.processorObj.isIdle(), "The processor shouldn't be idle")
 
-        self.assertEqual(queueObj.getQueueLength(), 0, "The queue must be empty")"""
+    def test_endService_empty_queue(self):
+        mock_input = Queue()
+        self.processorObj.addInput(mock_input)
+        self.processorObj.hostedEntity = 1
+
+        self.assertFalse(self.processorObj.isIdle(), "The processor shouldn't be idle")
+        self.processorObj.endService()
+        self.assertTrue(self.processorObj.isIdle(), "The processor should be idle")
+
+    def test_endService_non_empty_queue(self):
+        input = Queue()
+        input.addOutput(self.processorObj)
+        self.processorObj.addInput(input)
+        self.processorObj.hostedEntity = 1
+
+        input.nextArrival(Entity(Constants.DUAL))
+
+        self.assertFalse(self.processorObj.isIdle(), "The processor shouldn't be idle")
+        self.processorObj.endService()
+        self.assertFalse(self.processorObj.isIdle(), "The processor shouldn't be idle")
